48a171e3b24331d5d7f7cd51a175a3ad47345b78
lizardfrozm <v@boyko.co.ua>
2016-01-04 23:35:58 +0200

    String lesson1

---
+++ src/String1.java
@@ -0,0 +1,17 @@
+
+public class String1 {
+
+	public static void main(String[] args) {
+		String x = "I`m writing some text";
+		System.out.println(x); //1.write any string
+		String y = " Java";
+		String text = "";
+		System.out.println(text); //5.write empty string
+		text = x+y; //2. using conkatenation
+		System.out.print(text); //4. using print
+		text = text+ 8; //3. Add digits
+		System.out.print(text);
+		
+	}
+
+}
